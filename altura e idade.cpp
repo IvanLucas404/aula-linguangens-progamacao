ccbfed365619fa054b8c9f044513bbed901ea421
IvanLucas404 <ivan.lucas.etech@gmail.com>
2024-04-05 20:34:34 -0400

    Create altura e idade.cpp

---
+++ altura e idade.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int idade = 0;
+    float altura = 0;
+    
+    cin >> idade;
+    cout << "Idade: " << idade << endl;
+    
+    cin >> altura;
+    cout << "Altura : " << altura << endl;
+    
+    return 0;
+}
